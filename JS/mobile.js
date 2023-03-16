d3a1f2da4238bacaea3d3a29d7d9f39875d71d9b
Matheus Petrakis <86805775+Petrakis20@users.noreply.github.com>
2023-03-15 22:40:08 -0300

    media-query

---
+++ JS/mobile.js
@@ -0,0 +1,15 @@
+// const ajuste = document.querySelectorAll('.ajuste');
+// console.log(ajuste)
+
+// // Verifica se o media query está sendo aplicado
+// const isSmallScreen = window.matchMedia('(max-width: 414px)').matches;
+
+// // Se o media query estiver sendo aplicado, altera a cor do fundo para azul
+// if (isSmallScreen) {
+//     ajuste.forEach(elemento => {
+//         elemento.classList.remove('col-md-auto')
+//         elemento.classList.add('col')
+//         elemento.classList.add('mt-2')
+//       });
+//   a
+// }
